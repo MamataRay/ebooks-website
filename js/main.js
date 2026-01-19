cc9e6528687798570ae7ee085db30572ad77a88b
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2026-01-19 22:38:06 +0530

    Implement mobile navigation menu with toggle and close functionality

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ if (searchClose) {
     })
 }
 
-
 // login
 const loginButton = document.getElementById('login-button'),
     loginClose = document.getElementById('login-close'),
@@ -36,6 +35,38 @@ if (loginClose) {
         loginContent.classList.remove('show-login')
     })
 }
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 //add shadow header
 const shadowheader = () => {
